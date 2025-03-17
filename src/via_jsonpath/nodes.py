9f094a666966a194b501655d6c5c86357e5af294
Andy Armstrong <andy@hexten.net>
2025-03-17 19:44:26 +0000

    Minor

--- src/via_jsonpath/nodes.py
+++ src/via_jsonpath/nodes.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass, field
-from functools import cached_property
 from typing import Any, Generator, Self
 
 from .jp import JP, JPField, JPRoot, JPSearch, JPWild
@@ -13,13 +12,6 @@ class Node:
     next: dict[JPField, Self] = field(default_factory=dict)
     leaf = False
 
-    @cached_property
-    def items(self) -> list[tuple[JPField, Self]]:
-        def key_fun(x):
-            return type(x[0]).__name__, x[0]
-
-        return sorted(self.next.items(), key=key_fun)
-
     def search(self, obj: Any, path) -> NodeWalk:
         yield from self.visit(obj, path)
         for idx, value in kv_of(obj):
@@ -30,7 +22,7 @@ class Node:
             yield path, obj, self
 
         if isinstance(obj, dict) or isinstance(obj, list):
-            for key, node in self.items:
+            for key, node in self.next.items():
                 if key == JPSearch:
                     yield from node.search(obj, path)
                 elif key == JPWild:
