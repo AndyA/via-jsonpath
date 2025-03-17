3093a1f67924748cd55eba5222c8b82e24a51e4f
Andy Armstrong <andy@hexten.net>
2025-03-17 21:14:16 +0000

    Minor

--- src/via_jsonpath/__init__.py
+++ src/via_jsonpath/__init__.py
@@ -1,4 +1,4 @@
-from .editor import Editor
-from .jp import JP, JPError, JPField, JPSearch, JPTuple, JPWild
-from .trie import Node, Trie
-from .via import Rule, Via, ViaContext
+from .editor import Editor  # noqa: F401
+from .jp import JP, JPError, JPField, JPSearch, JPTuple, JPWild  # noqa: F401
+from .trie import Node, Trie  # noqa: F401
+from .via import Rule, Via, ViaContext  # noqa: F401
