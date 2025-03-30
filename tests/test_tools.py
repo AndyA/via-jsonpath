666a88b06ec6d3a55285d5f3f2f4c322af5920f7
Andy Armstrong <andy@hexten.net>
2025-03-30 20:05:50 +0100

    Add tools inc scan

---
+++ tests/test_tools.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from typing import Any
+
+from via_jsonpath import JP, scan
+
+
+@dataclass(kw_only=True, frozen=True)
+class ScanCase:
+    obj: Any
+    want: list[tuple[Any, JP]]
+    inner: bool = False
+
+    def check(self):
+        got = list(scan(self.obj, inner=self.inner))
+        print(f"want: {self.want}")
+        print(f"got: {got}")
+        assert got == self.want
+
+
+def test_scan():
+    test_cases = [
+        ScanCase(
+            obj={"foo": 1},
+            want=[(1, JP("$.foo"))],
+        ),
+        ScanCase(
+            obj=[{"foo": 1}],
+            want=[(1, JP("$[0].foo"))],
+        ),
+        ScanCase(
+            obj=[{"foo": 1}, {"bar": 2}],
+            want=[(1, JP("$[0].foo")), (2, JP("$[1].bar"))],
+        ),
+        ScanCase(
+            obj=[{"foo": 1}, {"bar": 2}],
+            inner=True,
+            want=[
+                ([{"foo": 1}, {"bar": 2}], JP("$")),
+                (1, JP("$[0].foo")),
+                (2, JP("$[1].bar")),
+            ],
+        ),
+    ]
+    for case in test_cases:
+        case.check()
